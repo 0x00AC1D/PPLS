deea7c0e5068073e234299fef583b04b40a78700
Samuel Havelka <samuelhavelka@seznam.cz>
2019-01-25 03:59:43 +0100

    replace <range object>.__contains__() in conflict condition with int.__ge__()

--- class_fitness_method.py
+++ class_fitness_method.py
@@ -14,9 +14,9 @@ from itertools import combinations
                                                          x.course.group.id == z.course.group.id), classes)
 
             classes = list(set(classes).difference(potential_conflict_group))
-            number_of_conflicts += sum(x.timeslot.start_time in range(y.timeslot.start_time, y.timeslot.end_time) or
-                                    y.timeslot.start_time in range(x.timeslot.start_time, x.timeslot.end_time) 
-                                    for x, y in combinations(potential_conflict_group))
+            number_of_conflicts += sum(not ((x.timeslot.start_time >= y.timeslot.end_time) or
+                                            (y.timeslot.start_time >= x.timeslot.end_time)) 
+                                            for x, y in combinations(potential_conflict_group))
 
         # Return a normalized fitness value between [0, 1]
         return 1 / (number_of_conflicts + 1)
